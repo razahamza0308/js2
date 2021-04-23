d00b5c0f89c5ff0745fddfd4dcdd7e6a28b1523e
razahamza0308 <omer91999@gmail.com>
2021-04-24 00:36:37 +0500

    cc

---
+++ app.js
@@ -0,0 +1,30 @@
+//******chapter 5 */
+
+// var a = 3
+// var b = 5
+// c=a+b
+// document.write("sum of " +a+ " and " +b +" is "+c)
+
+
+// var a = 3
+// var b = 5
+// c=a-b
+// document.write("Substraction of " +a+ " and " +b +" is "+c)
+
+
+// var a = 3
+// var b = 5
+// c=a*b
+// document.write("Multipilcation of " +a+ " and " +b +" is "+c)
+
+
+// var a = 3
+// var b = 5
+// c=a%b
+// document.write("modulus of " +a+ " and " +b +" is "+c)
+
+
+var a = 3
+var b = 5
+c=a/b
+document.write("Division of " +a+ " and " +b +" is "+c)
